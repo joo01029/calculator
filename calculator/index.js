c7a0e5cc39e012a875ef92b33958cb7f0188164b
joo01029 <joo01029@naver.com>
2020-09-01 17:47:44 +0900

    함수 수정

--- calculator/index.js
+++ calculator/index.js
@@ -1,51 +1,18 @@
 var sum = 0;
 var sum_all = 0;
 var sign = "+";
+var first = true;
 i = 0;
 function sum_print(){
     cal = document.getElementById("cal");
-    cal.innerHTML = '<p style="font-size:30px;">'+sum_all+((sum != 0)?sign+sum:"")+'</p>';
+    cal.innerHTML = '<p style="font-size:30px;text-align: right;">'+(first == true?"":sum_all)+((sum != 0)?sign+sum:"")+'</p>';
 }
-function one(){
-    sum = (sum*10)+1;
+
+function number(num){
+    sum = (sum*10)+num;
     sum_print()
 }
-function two(){
-    sum = (sum*10)+2;
-    sum_print()
-}
-function thr(){
-    sum = (sum*10)+3;
-    sum_print()
-}
-function four(){
-    sum = (sum*10)+4;
-    sum_print()
-}
-function fiv(){
-    sum = (sum*10)+5;
-    sum_print()
-}
-function six(){
-    sum = (sum*10)+6;
-    sum_print()
-}
-function sev(){
-    sum = (sum*10)+7;
-    sum_print()
-}
-function eig(){
-    sum = (sum*10)+8;
-    sum_print()
-}
-function nin(){
-    sum = (sum*10)+9;
-    sum_print()
-}
-function zr(){
-    sum = (sum*10);
-    sum_print()
-}
+
 function result(){
     if(sum == 0){
         sum_print()
@@ -65,6 +32,7 @@ function result(){
         sum = 0;
         sum_print()
         sum_all = 0;
+        first = true;
     }
 }
 function add(){
@@ -84,7 +52,9 @@ function add(){
         }
         sign = "+";
         sum = 0;
+        first = false;
         sum_print()
+        
     }
     
 }
@@ -105,6 +75,8 @@ function sub(){
         }
         sign = "-";
         sum = 0;
+        
+        first = false;
         sum_print()
     }
     
@@ -127,6 +99,9 @@ function mul(){
         sign = "X";
         sum = 0;
         sum_print()
+        first = false;
+        
+        
     }
     
 }
@@ -147,7 +122,9 @@ function divi(){
         }
         sign = "/";
         sum = 0;
+        first = false;
         sum_print()
+        
     }
     
 }
@@ -155,5 +132,7 @@ function C(){
     sum_all = 0;
     sum = 0;
     sign = "+";
+    first = true;
     sum_print()
+    
 }
